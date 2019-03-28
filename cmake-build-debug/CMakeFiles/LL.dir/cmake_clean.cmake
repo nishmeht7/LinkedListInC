d5bff3e3c0fa3257d4f47973b3ccb1ec74f9c3e6
Nishant Mehta <nishmeht7@gmail.com>
2019-03-28 15:15:22 -0700

    insertion LL working

---
+++ cmake-build-debug/CMakeFiles/LL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LL.dir/main.c.o"
+  "LL.pdb"
+  "LL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "helper.h"
+
+void printLL(node *head) {
+    node *curr = head;
+    while(curr != NULL) {
+        printf("%d\t", curr->data);
+        curr = curr->next;
+    }
+    printf("\n");
+}
+
+node* createList(int n) {
+    int i = 0;
+    node *temp;
+    node *head = NULL;
+    node *tail = NULL;
+//    node *mainList;
+
+    for(i = 0; i < n; i++) {
+        printf("inside\n");
+        temp = (node*)malloc(sizeof(node));
+        printf("Please enter a number to add: \n");
+        scanf(" %d", &(temp->data));
+        temp->next = NULL;
+        printf("the num is: %d\n", temp->data);
+        printf("head is: %p\n", head);
+//
+        if(head == NULL) {
+            printf("inside if");
+            head = temp;
+            tail = temp;
+        }
+        else {
+            tail->next = temp;
+            tail = temp;
+        }
+    }
+    return head;
+}
+
+int main() {
+    int nodes = 0;
+    printf("How many nodes would you like to add? ");
+    scanf("%d", &nodes);
+
+    node *new = createList(nodes);
+    printLL(new);
+    return 0;
+}
