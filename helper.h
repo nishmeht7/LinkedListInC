c11138ac1aa39050fb8d835c11695700c834dc80
Nishant Mehta <nishmeht7@gmail.com>
2019-04-01 10:45:19 -0700

    removed commented out

--- helper.h
+++ helper.h
@@ -1,20 +1,3 @@
-//struct node {
-//    union {
-//        int i;
-//        double d;
-//        char str[20];
-//        void *ptr;
-//    } data;
-//
-//    //string version of the data
-//    char dataStr[20];
-//    int type; //1 =int, 2 = double, 3 = string
-//    union data;
-//    struct node* next;
-//}node_default = {NULL, 0, NULL, NULL};
-//
-//typedef struct node node;
-
 struct node {
     void *data;
     char dataStr[20];
@@ -23,7 +6,4 @@ struct node {
 }node_default = {NULL, "\0", 0, NULL};
 
 typedef struct node node;
-
-
-
 
