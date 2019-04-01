c603e089132238d22e211e3ce4a4473eb71ecba2
Nishant Mehta <nishmeht7@gmail.com>
2019-04-01 10:41:46 -0700

    node structure changed to void pointer

--- helper.h
+++ helper.h
@@ -1,18 +1,29 @@
-struct node {
-    union {
-        int i;
-        double d;
-        char str[20];
-    } data;
+//struct node {
+//    union {
+//        int i;
+//        double d;
+//        char str[20];
+//        void *ptr;
+//    } data;
+//
+//    //string version of the data
+//    char dataStr[20];
+//    int type; //1 =int, 2 = double, 3 = string
+//    union data;
+//    struct node* next;
+//}node_default = {NULL, 0, NULL, NULL};
+//
+//typedef struct node node;
 
-    //string version of the data
+struct node {
+    void *data;
     char dataStr[20];
-    int type; //1 =int, 2 = double, 3 = string
-    union data;
+    int type; //1 =int, 2 = double, 3 = string 4 = list
     struct node* next;
-}node_default = {NULL, 0, NULL, NULL};
+}node_default = {NULL, "\0", 0, NULL};
 
 typedef struct node node;
+
 
 
 
--- main.c
+++ main.c
@@ -6,13 +6,16 @@
 
 #define MAX_INPUT 100
 
+node* createList();
+node* createListWrapper();
+
 /**
  * prints a linked list
  * @param head first node of the list
  */
 void printLL(node *head) {
     while(head != NULL) {
-        printf("%s\t", &head->dataStr);
+        printf("%s\t", head->dataStr);
         head = head->next;
     }
     printf("\n");
@@ -44,27 +47,39 @@ node* createNode(char *newData) {
 
     node *temp = (node*)malloc(sizeof(node));
     strcpy(temp->dataStr, newData);
-
     if(sscanf(newData, "%lf", &d) == 1) {
         i = (int)d; // typecast to int.
         if (fabs(d - i) / d > tolerance) {
             printf("The input is a floating point\n");
-            temp->data.d = d;
+            temp->data = &d;
             temp->type = 2;
         }
         else {
             printf("The input is an integer\n");
-            temp->data.i = i;
+            temp->data = &i;
             temp->type = 1;
         }
     }
     else {
         printf("The input is a string\n");
-        strcpy(temp->data.str, newData);
+//        strcpy(temp->data.str, newData);
+        temp->data = newData;
         temp->type = 3;
     }
 
     return temp;
+}
+
+
+node* createListWrapper() {
+    char userInput[MAX_INPUT];
+    int nodes = 0;
+
+    printf("How many nodes would you like to add? ");
+    fgets(userInput, MAX_INPUT, stdin);
+    nodes = strtol(userInput, NULL, 10);
+    printf("nodes are: %d\n", nodes);
+    return createList(nodes);
 }
 
 /**
@@ -80,12 +95,26 @@ node* createList(int n) {
 
     for(i = 0; i < n; i++) {
         printf("(inside list)\n");
-        printf("Please enter a value to add: ");
-        char inputData[MAX_INPUT];
-        char *newData = inputData;
-        fgets(newData, MAX_INPUT, stdin);
-        newData[strcspn(newData, "\n")] = 0; // removes new line from fgets buffer
-        currNode = createNode(newData);
+//        printf("Enter 1 to add a value and 2 to add a list");
+//        char inputData1[MAX_INPUT];
+//        char *inputPtr = inputData1;
+//        fgets(inputPtr, MAX_INPUT, stdin);
+//        inputPtr[strcspn(inputPtr, "\n")] = 0;
+////        int userInput = (int)(inputPtr);
+//        int userInput = strtol(inputData1, NULL, 10);
+//        printf("the input ptr is: %s\n", inputPtr);
+//        printf("the use input is: %d\n", userInput);
+//        if(userInput == 2) {
+//            currNode = createListWrapper();
+//        }
+//        else {
+            printf("Please enter a value to add: ");
+            char inputData[MAX_INPUT];
+            char *newData = inputData;
+            fgets(newData, MAX_INPUT, stdin);
+            newData[strcspn(newData, "\n")] = 0; // removes new line from fgets buffer
+            currNode = createNode(newData);
+//        }
 
         if(head == NULL) {
             head = currNode;
@@ -125,6 +154,37 @@ node* insert(node *head, int idx, char *str) {
     }
     return head;
 }
+
+/**
+ * inserts new element into list
+ * @param head head of the list to insert into
+ * @param idx position to insert at
+ * @param str data of new node
+ * @return head of list inserted into
+ */
+node* insertNode(node *head, node* elem, int idx) {
+    node *curr = head;
+    while(curr != NULL) {
+        printf("curr is: %s\n", curr->dataStr);
+        printf("index is: %d\n", idx);
+
+        if(idx == 0) {
+            elem->next = head;
+            return elem;
+        }
+        else if(idx - 1 == 0) {
+            printf("curr and elem %s\t%s\n", curr->dataStr, elem->dataStr);
+            node *temp = curr->next;
+            curr->next = elem;
+            elem->next = temp;
+            break;
+        }
+        idx--;
+        curr = curr->next;
+    }
+    return head;
+}
+
 
 /**
  * appends an item to the end of the list
@@ -167,6 +227,65 @@ node* concat(node *list1, node *list2) {
     return list1;
 }
 
+node* getNode(int pos, node* head) {
+    node *curr = head;
+    while(pos != 0) {
+        if(curr == NULL) return NULL;
+        curr = curr->next;
+        pos --;
+    }
+    return curr;
+}
+
+node* sort(node *head) {
+    node *newHead = head;
+    node *curr = head;
+    node *prev = curr;
+    node *prevCurr = NULL;
+    int pos = 0;
+    int minIdx = 0;
+    while(curr != NULL) {
+        printf("inside while\n");
+        printf("curr is: %s\n", curr->dataStr);
+        node *min = curr;
+        int innerIdx = pos;
+        if(curr->next != NULL) {
+            node *innerPrev = curr;
+            node *next = curr->next;
+            while(next != NULL) {
+                innerIdx++;
+                printf("inside inner while\n");
+                printf("next is: %s\n", next->dataStr);
+                int compare = strcmp(min->dataStr, next->dataStr);
+                if(compare > 0) {
+                    min = next;
+                    prev = innerPrev;
+                    minIdx = innerIdx;
+                }
+                next = next->next;
+                if(next != NULL) innerPrev = innerPrev->next;
+            }
+        }
+        printf("the previous is: %s\n", prev->dataStr);
+        printf("the min is: %s\n", min->dataStr);
+        if(min != curr) {
+            prev->next = min->next;
+            node *inserted = insertNode(head, min, pos);
+            if(pos == 0) newHead = inserted;
+            newHead->next = curr->next;
+            printf("inside if %s\n", curr->dataStr);
+            printf("MIN IDX is: %d\n", minIdx);
+            curr->next = NULL;
+
+            insertNode(newHead, curr, minIdx);
+        }
+        pos++;
+        prevCurr = curr;
+        curr = curr->next;
+    }
+    return newHead;
+}
+
 //TODO print, append(data), getLength, insert(index, data), modify(index, data)
 //TODO min, max, concat, reverse, sort (selection)
 
@@ -178,28 +297,43 @@ node* concat(node *list1, node *list2) {
  * @return
  */
 int main() {
-    char userInput[MAX_INPUT];
-    int nodes = 0;
+//    char userInput[MAX_INPUT];
+//    int nodes = 0;
+//
+//    printf("How many nodes would you like to add? ");
+//    fgets(userInput, MAX_INPUT, stdin);
+//
+//    nodes = atoi(userInput);
+//    node *new = createList(nodes);
+////
+////    printf("How many nodes would you like for the second list? ");
+////    fgets(userInput, MAX_INPUT, stdin);
+////
+////    nodes = atoi(userInput);
+////    node *second = createList(nodes);
 
-    printf("How many nodes would you like to add? ");
-    fgets(userInput, MAX_INPUT, stdin);
+    node *new = createListWrapper();
 
-    nodes = atoi(userInput);
-    node *new = createList(nodes);
-
-    printf("How many nodes would you like for the second list? ");
-    fgets(userInput, MAX_INPUT, stdin);
-
-    nodes = atoi(userInput);
-    node *second = createList(nodes);
+//    node *one;
+//    node *two;
+//    int oneNum = 6;
+//    int *onePtr = &oneNum;
+//    int twoNum = 5;
+//    int *twoPtr = &twoNum;
+//    two = &(node){.data = twoPtr, .dataStr = "5", .type = 1, .next = NULL};
+//    one = &(node){.data = onePtr, .dataStr = "6", .type = 1, .next = two};
 
     printLL(new);
 
+//    node *sorted = sort(new);
+
+//    printLL(sorted);
+
 //    node *newHead = insert(new, 2, "[1,2,3,4]");
 //    node *newHead = append(new, "54321");
-    node *newHead = concat(new, second);
+//    node *newHead = concat(new, second);
 
-    printLL(newHead);
+//    printLL(newHead);
 
     return 0;
 }
